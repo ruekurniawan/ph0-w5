e8a92e4c1c5b9c69078b40afad940048976714d5
ruekurniawan <ruekurniawan>
2019-01-27 12:10:54 +0700

    Tugas Kamis

---
+++ Kamis/hapusSimbol.js
@@ -0,0 +1,16 @@
+function hapusSimbol(str) {
+  var string = str.match(/[a-z0-9]+/gi);
+  var result = "";
+  for(var i = 0; i < string.length; i++)
+  {
+  	result += string[i]; 
+  }
+  return result;
+}
+
+// TEST CASES
+console.log(hapusSimbol('test%$4aa')); // test4aa
+console.log(hapusSimbol('devel0p3r s3j@@ati')); // devel0p3rs3jati
+console.log(hapusSimbol('ma@#k!an~')); // makan
+console.log(hapusSimbol('coding')); // coding
+console.log(hapusSimbol('1+3-5*2=100')); // 1352100
